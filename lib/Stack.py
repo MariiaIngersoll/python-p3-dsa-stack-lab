ed86556d2a70695147767412e3cd46ca4da87909
+ <+>
2023-06-25 17:36:30 -0400

    not fully complited because of the wrong tests

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,13 +1,20 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.limit = limit
+        self.items = items
 
     def isEmpty(self):
-        pass
+        if len(self.items) == 0:
+            return True
+        else:
+            return False
 
     def push(self, item):
-        pass
+        if len(self.items) >= self.limit:
+            raise ValueError("Stack is full")
+        else:
+            self.items.append(item)
 
     def pop(self):
         pass
@@ -16,10 +23,26 @@ class Stack:
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if len(self.items) >= self.limit:
+            return True
+        else: 
+            return False
 
     def search(self, target):
-        pass
+        if self.isEmpty():
+            return -1
+        
+        i = len(self.items) - 1
+        distance = 0
+
+        while i >= 0:
+            if self.items[i] == target:
+                return distance
+            i -= 1
+            distance += 1
+
+        return -1
+
